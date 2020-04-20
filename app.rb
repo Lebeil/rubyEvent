bcb5caa68deeea7310f566d504c134ebc327e881
Lebeil <liedel75@gmail.com>
2020-04-20 22:35:49 +0200

    fin de l'exo

--- app.rb
+++ app.rb
@@ -10,3 +10,17 @@ require_relative 'lib/event'
 
 # Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
 # Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
+julie = User.new("julie@gmail.com", 18)
+jean = User.new("jean@jean.com", 28)
+user_1 = User.new("claude@claude.com", 75)
+
+
+puts "Voici l'age du User trouvé : #{user_1.age}" 
+
+
+my_event = Event.new("2019-01-13 09:00", 10, "standup quotidien", [julie, jean])
+p my_event.title
+
+
+
+binding.pry
--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,36 @@
 class Event
 
+    attr_accessor :start_date, :duration, :title, :attendees
+
+    def initialize(start_date, duration, title, attendees)
+        @start_date = Time.parse(start_date) #=> 2010-10-31 12:00:00 +0100
+        @duration = duration #en minutes
+        @title = title
+        @attendees = attendees
+    end
+
+    def end_date
+        return @start_date + duration*60
+    end
+
+    def is_past?
+        return @start_date < Time.now
+    end
+
+    def is_future?
+        !self.is_past?
+    end
+
+    def is_soon?
+        return @start_date < Time.now + 60*30
+    end
+
+    def to_s
+        puts ">Titre: #{@title}"
+        puts ">Date de début: #{start_date}"
+        puts ">Durée: #{duration}"
+        print ">Invités :"
+        puts @attendees.join(", ")
+    end
+
 end
